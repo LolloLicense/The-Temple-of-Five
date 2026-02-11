736ad2acbd1f87d68987e6e6cee88976ba0bc27d
elychnell <elychnell@gmail.com>
2026-02-11 14:53:48 +0100

    Updated file structure + Small function import test

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,5 @@
-import './sass/style.scss'
+import './sass/style.scss';
+import { test2 } from './script/room1.ts';
+
+console.log('Running imported test2 from main.ts');
+test2();
---
+++ src/script/room1.ts
@@ -0,0 +1,6 @@
+import { test } from './utils.ts';
+
+export function test2() {
+	console.log('Running imported test from room1.ts');
+	test();
+}
