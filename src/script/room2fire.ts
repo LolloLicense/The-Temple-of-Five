f20ee99098a490c7a7bc93f1d34d707a1a7bd097
AlexJCodes <alexander.johansson8910@gmail.com>
2026-02-25 11:05:21 +0100

    update ts fireroom - string literals FireKey

--- src/script/room2fire.ts
+++ src/script/room2fire.ts
@@ -2,6 +2,10 @@ import { playBgm } from "../audio";
 import * as dataJSON from "../data.json";
 import { renderRoomDesc } from "./roomDesc";
 
+/**
+ * Hides welcome page - sets the background for the fire room - shows the fire room section - plays the background music for the fire room
+ */
+
 export function room2fireFunc(): void {
   /* Hide the welcome page (menu)
       This can be removed when we remove the menu */
@@ -14,9 +18,9 @@ export function room2fireFunc(): void {
   /* Sets the background for the room and shows room section */
   const fireSection: HTMLElement | null = document.querySelector("#room2Fire");
   if (!fireSection) return;
-    fireSection.style.backgroundImage = `url("${dataJSON.room2fire.backgroundImg}")`;
-    fireSection.classList.remove("hidden");
-    renderRoomDesc(fireSection, dataJSON.room2fire.desc);
+  fireSection.style.backgroundImage = `url("${dataJSON.room2fire.backgroundImg}")`;
+  fireSection.classList.remove("hidden");
+  renderRoomDesc(fireSection, dataJSON.room2fire.desc);
 
   /* Play the background music for the fire room */
   const bgmId = dataJSON.room2fire.bgmId;
@@ -27,3 +31,15 @@ export function room2fireFunc(): void {
   console.log("Hello from the fire room");
   console.log("Calling playBgm with:", bgmId);
 }
+
+/* -------------------------------------------------------------------------------------------------------------------------------------------------- */
+/* ------------------------------------------------------------- NEW SECTION ------------------------------------------------------------------------ */
+/* -------------------------------------------------------------------------------------------------------------------------------------------------- */
+
+/**
+ * String literal type for the fire room keys, which can be used to ensure type safety when referencing these keys in the code.
+ * A = Air, T = Timber, F = Flame, E = Ember, S = 
+ */
+
+type FireKey = "A" | "T" | "F" | "E" | "S" | "W";
+
